fac7068e43964e0fbc6bf6f0a4b6b669fbf23488
jrBordet <jean.bordet@vidiemme.it>
2022-08-26 16:21:33 +0200

    Update models

--- Sources/BrianzaGenerator/Models+.swift
+++ Sources/BrianzaGenerator/Models+.swift
@@ -9,20 +9,52 @@ import Foundation
 
 let dice_one = [
     0: "bo",
-    1: "bu"
+    1: "bu",
+    2: "la",
+    3: "ce",
+    4: "ma",
+    5: "bia",
+    6: "ve",
+    7: "ca",
+    8: "giu",
+    9: "lo"
 ]
 
 let dice_two = [
     0: "cherio",
-    1: "mate"
+    1: "mate",
+    2: "bbiate",
+    3: "nzate",
+    4: "rezzana",
+    5: "rago",
+    6: "sano",
+    7: "visio",
+    8: "giola",
+    9: "rate"
 ]
 
 let dice_three = [
     0: "co",
-    1: "va"
+    1: "va",
+    2: "su",
+    3: "stro",
+    4: "bia",
+    5: "oro",
+    6: "bo",
+    7: "ma",
+    8: "li",
+    9: "ge"
 ]
 
 let dice_four = [
     0: "sciago",
-    1: "reggio"
+    1: "reggio",
+    2: "ssone",
+    3: "regno",
+    4: "nzano",
+    5: "ssate",
+    6: "derno",
+    7: "ssono",
+    8: "scone",
+    9: "nago"
 ]
